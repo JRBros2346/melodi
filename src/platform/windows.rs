5c0f9cba254f2c3764431646f9c11690eb4dc503
JRBros2346 <jrbros2346@gmail.com>
2024-04-02 19:07:22 +0530

    Windows unsafe fix

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,13 +1,10 @@
-#![allow(unused_unsafe)]
-
 // Windows platform layer.
+use std::result::Result;
 use windows::core::*;
 use windows::Win32::Foundation::*;
 use windows::Win32::Graphics::Gdi::*;
 use windows::Win32::System::LibraryLoader::*;
 use windows::Win32::UI::WindowsAndMessaging::*;
-
-use std::result::Result;
 
 pub struct PlatformState {
     instance: HINSTANCE,
@@ -21,32 +18,30 @@ impl PlatformState {
         width: i32,
         height: i32,
     ) -> Result<Self, String> {
-        let mut out = Self {
-            instance: HINSTANCE::default(),
-            window: HWND::default(),
-        };
         unsafe {
+            let mut out = Self {
+                instance: HINSTANCE::default(),
+                window: HWND::default(),
+            };
             if let Err(e) = GetModuleHandleExW(0, None, &mut out.instance.into() as *mut _) {
                 return Err(e.to_string());
             }
-        }
 
-        // Setup and register window class.
-        if unsafe {
-            RegisterClassExW(&WNDCLASSEXW {
+            // Setup and register window class.
+            if RegisterClassExW(&WNDCLASSEXW {
                 cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
                 style: CS_DBLCLKS, // Get double-clicks
                 lpfnWndProc: Some(Self::win32_process_messages),
                 cbClsExtra: 0,
                 cbWndExtra: 0,
                 hInstance: out.instance,
-                hIcon: match unsafe { LoadIconW(out.instance, IDI_APPLICATION) } {
+                hIcon: match LoadIconW(out.instance, IDI_APPLICATION) {
                     Ok(icon) => icon,
                     Err(e) => {
                         return Err(e.to_string());
                     }
                 },
-                hCursor: match unsafe { LoadCursorW(None, IDC_ARROW) } {
+                hCursor: match LoadCursorW(None, IDC_ARROW) {
                     // None: Manage the cursor manually
                     Ok(cur) => cur,
                     Err(e) => {
@@ -58,57 +53,52 @@ impl PlatformState {
                 lpszClassName: w!("strings_window_class"),
                 hIconSm: HICON(0),
             } as *const _)
-        } == 0
-        {
-            unsafe {
+                == 0
+            {
                 MessageBoxW(
                     None,
                     w!("Window registration failed"),
                     w!("Error"),
                     MB_ICONEXCLAMATION | MB_OK,
                 );
+                return Err(Error::from_win32().to_string());
             }
-            return Err(Error::from_win32().to_string());
-        }
 
-        // Create window
-        let client_x = x as u32;
-        let client_y = y as u32;
-        let client_width = width as u32;
-        let client_height = height as u32;
+            // Create window
+            let client_x = x as u32;
+            let client_y = y as u32;
+            let client_width = width as u32;
+            let client_height = height as u32;
 
-        let mut window_x = client_x;
-        let mut window_y = client_y;
-        let mut window_width = client_width;
-        let mut window_height = client_height;
+            let mut window_x = client_x;
+            let mut window_y = client_y;
+            let mut window_width = client_width;
+            let mut window_height = client_height;
 
-        let mut window_style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION;
-        let window_ex_style = WS_EX_APPWINDOW;
+            let mut window_style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION;
+            let window_ex_style = WS_EX_APPWINDOW;
 
-        window_style |= WS_MAXIMIZEBOX;
-        window_style |= WS_MINIMIZEBOX;
-        window_style |= WS_THICKFRAME;
+            window_style |= WS_MAXIMIZEBOX;
+            window_style |= WS_MINIMIZEBOX;
+            window_style |= WS_THICKFRAME;
 
-        // Obtain the size of the border.
-        let mut border = RECT::default();
-        unsafe {
+            // Obtain the size of the border.
+            let mut border = RECT::default();
             if let Err(e) =
                 AdjustWindowRectEx(&mut border as *mut _, window_style, None, window_ex_style)
             {
                 return Err(e.to_string());
             }
-        }
 
-        // In this case, the border rectangle is negative.
-        window_x = (window_x as i32 + border.left) as u32;
-        window_y = (window_y as i32 + border.top) as u32;
+            // In this case, the border rectangle is negative.
+            window_x = (window_x as i32 + border.left) as u32;
+            window_y = (window_y as i32 + border.top) as u32;
 
-        // Grow by the size of the OS border.
-        window_width = (window_width as i32 + (border.right - border.left)) as u32;
-        window_height = (window_height as i32 + (border.bottom - border.top)) as u32;
+            // Grow by the size of the OS border.
+            window_width = (window_width as i32 + (border.right - border.left)) as u32;
+            window_height = (window_height as i32 + (border.bottom - border.top)) as u32;
 
-        out.window = unsafe {
-            CreateWindowExW(
+            out.window = CreateWindowExW(
                 window_ex_style,
                 w!("strings_window_class"),
                 PCWSTR::from_raw(
@@ -127,24 +117,20 @@ impl PlatformState {
                 None,
                 out.instance,
                 None,
-            )
-        };
-        if out.window.0 == 0 {
-            unsafe {
+            );
+            if out.window.0 == 0 {
                 MessageBoxW(
                     None,
                     w!("Window creation failed!"),
                     w!("Error!"),
                     MB_ICONEXCLAMATION | MB_OK,
                 );
+                crate::fatal!("Window creation failed!");
+                return Err(Error::from_win32().to_string());
             }
-            crate::fatal!("Window creation failed!");
-            return Err(Error::from_win32().to_string());
-        }
 
-        // Show the window
-        let should_activate = true; // TODO: if the window should not accept input, this should be `false`.
-        unsafe {
+            // Show the window
+            let should_activate = true; // TODO: if the window should not accept input, this should be `false`.
             ShowWindow(
                 out.window,
                 // If initially minimized, use `{SW_MINIMIZE} else {SW_SHOWMINNOACTIVE}`
@@ -155,31 +141,31 @@ impl PlatformState {
                     SW_SHOWNOACTIVATE
                 },
             );
-        }
-        colored::control::set_virtual_terminal(true).unwrap();
+            colored::control::set_virtual_terminal(true).unwrap();
 
-        Ok(out)
+            Ok(out)
+        }
     }
     pub fn shutdown(&mut self) -> Result<(), String> {
-        if self.window.0 != 0 {
-            unsafe {
+        unsafe {
+            if self.window.0 != 0 {
                 if let Err(e) = DestroyWindow(self.window) {
                     return Err(e.to_string());
                 }
+                self.window = HWND::default();
             }
-            self.window = HWND::default();
+            Ok(())
         }
-        Ok(())
     }
     pub fn pump_messages(&self) -> Result<bool, String> {
-        let mut message = MSG::default();
-        while unsafe { PeekMessageW(&mut message as *mut _, None, 0, 0, PM_REMOVE) }.0 > 0 {
-            unsafe {
+        unsafe {
+            let mut message = MSG::default();
+            while PeekMessageW(&mut message as *mut _, None, 0, 0, PM_REMOVE).0 > 0 {
                 TranslateMessage(&message as *const _);
                 DispatchMessageW(&message as *const _);
             }
+            Ok(true)
         }
-        Ok(true)
     }
     unsafe extern "system" fn win32_process_messages(
         window: HWND,
