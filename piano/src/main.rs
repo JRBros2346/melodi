545e9bb925dfbc87cc02291412fc5092d3a50948
JRBros2346 <jrbros2346@gmail.com>
2024-11-05 11:18:02 +0530

    Platform Layer...

--- piano/src/main.rs
+++ piano/src/main.rs
@@ -1,13 +1,16 @@
 use strings::*;
 
 fn main() {
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::FmtSubscriber::new(),
-    )
-    .unwrap();
+    tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new()).unwrap();
     error!("ERROR");
     warn!("WARN");
     info!("INFO");
     debug!("DEBUG");
     trace!("TRACE");
+
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+    let mut platform_state = PlatformState::default();
+    event_loop.run_app(&mut platform_state).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,51 @@
 pub use tracing::{self, debug, error, info, trace, warn};
+pub use winit;
+
+#[derive(Default)]
+pub struct PlatformState {
+    window: Option<winit::window::Window>,
+}
+
+impl winit::application::ApplicationHandler for PlatformState {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(winit::window::Window::default_attributes())
+                .unwrap(),
+        )
+    }
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            winit::event::WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            winit::event::WindowEvent::Destroyed => {}
+            winit::event::WindowEvent::Resized(size) => {}
+            winit::event::WindowEvent::KeyboardInput {
+                device_id,
+                event,
+                is_synthetic,
+            } => {}
+            winit::event::WindowEvent::CursorMoved {
+                device_id,
+                position,
+            } => {}
+            winit::event::WindowEvent::MouseWheel {
+                device_id,
+                delta,
+                phase,
+            } => {}
+            winit::event::WindowEvent::MouseInput {
+                device_id,
+                state,
+                button,
+            } => {}
+            _ => (),
+        }
+    }
+}
