cd75109f3c789b26dff9355f636deb713417c85d
JRBros2346 <jrbros2346@gmail.com>
2024-11-06 10:04:22 +0530

    Platform Layer Complete

--- piano/src/main.rs
+++ piano/src/main.rs
@@ -1,4 +1,5 @@
 use strings::*;
+use winit::dpi::{PhysicalPosition, PhysicalSize};
 
 fn main() {
     tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new()).unwrap();
@@ -11,6 +12,13 @@ fn main() {
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
 
-    let mut platform_state = PlatformState::default();
+    let mut platform_state = PlatformState::new(
+        "Piano",
+        PhysicalPosition { x: 100, y: 100 },
+        PhysicalSize {
+            width: 1280,
+            height: 720,
+        },
+    );
     event_loop.run_app(&mut platform_state).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,40 @@
 pub use tracing::{self, debug, error, info, trace, warn};
 pub use winit;
+use winit::dpi::{Position, Size};
 
-#[derive(Default)]
 pub struct PlatformState {
     window: Option<winit::window::Window>,
+    title: String,
+    position: Position,
+    size: Size,
+}
+
+impl PlatformState {
+    pub fn new<T, P, S>(title: T, position: P, size: S) -> Self
+    where
+        T: Into<String>,
+        P: Into<Position>,
+        S: Into<Size>,
+    {
+        Self {
+            window: None,
+            title: title.into(),
+            position: position.into(),
+            size: size.into(),
+        }
+    }
 }
 
 impl winit::application::ApplicationHandler for PlatformState {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         self.window = Some(
             event_loop
-                .create_window(winit::window::Window::default_attributes())
+                .create_window(
+                    winit::window::Window::default_attributes()
+                        .with_title(&self.title)
+                        .with_position(self.position)
+                        .with_inner_size(self.size),
+                )
                 .unwrap(),
         )
     }
@@ -21,30 +45,41 @@ impl winit::application::ApplicationHandler for PlatformState {
         event: winit::event::WindowEvent,
     ) {
         match event {
+            winit::event::WindowEvent::RedrawRequested => {}
             winit::event::WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
             winit::event::WindowEvent::Destroyed => {}
-            winit::event::WindowEvent::Resized(size) => {}
+            winit::event::WindowEvent::Resized(size) => {
+                // todo!("window resize");
+            }
             winit::event::WindowEvent::KeyboardInput {
                 device_id,
                 event,
                 is_synthetic,
-            } => {}
+            } => {
+                // todo!("input processing");
+            }
             winit::event::WindowEvent::CursorMoved {
                 device_id,
                 position,
-            } => {}
+            } => {
+                // todo!("input processing");
+            }
             winit::event::WindowEvent::MouseWheel {
                 device_id,
                 delta,
                 phase,
-            } => {}
+            } => {
+                // todo!("input processing")
+            }
             winit::event::WindowEvent::MouseInput {
                 device_id,
                 state,
                 button,
-            } => {}
+            } => {
+                // todo!("input processing");
+            }
             _ => (),
         }
     }
